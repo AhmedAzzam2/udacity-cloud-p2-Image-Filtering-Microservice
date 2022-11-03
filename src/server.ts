fa50e4f337e5f0c973f7d617438f8f6aa0618edb
AhmedAzzam2 <90281469+AhmedAzzam2@users.noreply.github.com>
2022-11-03 16:01:39 +0200

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,34 @@
-import express from 'express';
+// import express from 'express'; typescript
+import express, { Express, Request, Response } from 'express';
+
+
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
 
+
   // Init the Express application
-  const app = express();
+  // const app = express(); changed to: typescript 
+  const app: Express = express();
 
   // Set the network port
-  const port = process.env.PORT || 8082;
+  const port: any = process.env.PORT || 8082;
   
   // Use the body parser middleware for post requests
+  // app.use(bodyParser.json()); changed to typescript
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  
+  
+  // @TODO2 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
 
-  //! END @TODO1
-  app.get( "/filteredimage", async ( req, res ) => {
-    let { image_url } = req.query;
+ let image_url: string = '';
+  app.get( "/filteredimage", async ( req: Request, res: Response ) => {
+    let { image_url } :{image_url:string} = req.query
     if ( !image_url ) {
       return res.status(400)
         .send(`image_url is required as a query parameter`);
@@ -44,12 +41,13 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
     } catch (error) {
       res.status(422).send(`Unable to process image`);
     }
-  } );  
+  } );
+
 
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+app.get('/', async(req: Request, res: Response) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
